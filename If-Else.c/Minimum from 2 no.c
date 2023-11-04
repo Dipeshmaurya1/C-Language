0e9db63633a16adb25ace5c1f75e7a93e2b3977d
Dipesh Maurya <149373441+Dipeshmaurya1@users.noreply.github.com>
2023-11-04 18:25:11 +0530

    Add files via upload

---
+++ If-Else.c/Minimum from 2 no.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a,b;
+		printf("Enter The Value of A:");
+		scanf("%d",&a);
+		printf("Enter The Value of B:");
+		scanf("%d",&b);
+	
+	if(a>b)
+	{
+		printf("The Minimum is B");
+	}
+	else
+	{
+		printf("The Minimum is A");
+	}
+}
---
+++ If-Else.c/Neutral No.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a;
+		printf("Enter The Value of A:");
+		scanf("%d",&a);
+		
+	if(a==0)
+	{
+		printf("Your Value is Neutral");
+	}
+	else
+	{
+		printf("Your Value is Not Neutral");
+	}
+}
